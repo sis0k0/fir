a443b34c34c140a2ceddfaa1ea7e422876bd7b89
sis0k0 <stanimira.vlaeva@gmail.com>
2018-12-23 00:14:20 +0200

    remove extraneous console.log

--- star.js
+++ star.js
@@ -2,7 +2,6 @@ import fir from './tree.js';
 
 function getStarText(rowsCount) {
     const blanks = Array(rowsCount * 2 + 1).join('&nbsp;');
-    console.log(blanks);
     const star = '<span class="star">*</span>';
 
     return `${blanks}${star}<br />`;

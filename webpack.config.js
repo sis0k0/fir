94d6068b9926b991ee971caaea04b6b9e64e1b48
sis0k0 <stanimira.vlaeva@gmail.com>
2018-12-28 17:06:34 +0200

    add webpack config to enable HMR

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
+
